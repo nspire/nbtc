d87972491cf485fbe1e44745d9f66cd4f0f564ea
Ilya Motamedi <ilyamotamedi@Ilyas-MBP.lan>
2017-12-03 03:12:18 -0500

    update

--- js/index.js
+++ js/index.js
@@ -3,5 +3,14 @@ $(document).ready(function () {
     var scroll = new SmoothScroll('a[href*="#"]');
 
     // Bootstrap scrollspy for nav link highlighting
-    $('body').scrollspy({ target: '#nav-links' })
+    $('body').scrollspy({ target: '#nav-links' });
+
+    // Collapse navbar on anchor selection
+    $('.nav-link').click(function() {
+        $('.navbar-collapse').collapse("hide");
+    });
+
+    $('.jumbotron').click(function() {
+        $('.navbar-collapse').collapse("hide");
+    });
 });
