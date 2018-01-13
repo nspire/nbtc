51b4a8ea1578ff05dc42481286c9bc883af5f8dc
Ilya Motamedi <ilya.motamedi@gmail.com>
2018-01-13 15:23:12 -0500

    added countdown

---
+++ js/countdown.js
@@ -0,0 +1,16 @@
+let confDate = new Date("March 10, 2018 09:00:00").getTime();
+
+let countDown = setInterval(() => {
+    let now = new Date().getTime(),
+        dt = confDate - now,
+        d = Math.floor(dt / (1000 * 60 * 60 * 24)),
+        h = Math.floor((dt % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        m = Math.floor((dt % (1000 * 60 * 60)) / (1000 * 60)),
+        s = Math.floor((dt % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = d + " days, " + h + "h, "+ m + "m, " + s + "s";
+    if (dt < 0) {
+        clearInterval(countDown);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000)
