3bd25a484ea587f7bba8899b7f59b98ed7177953
Ilya Motamedi <ilya.motamedi@gmail.com>
2018-01-15 15:10:24 -0500

    cleaned up

--- js/countdown.js
+++ js/countdown.js
@@ -12,10 +12,9 @@ let timeTo = (date) => {
     result.s = Math.floor((dt % (1000 * 60)) / 1000);
     if (dt < 0) return 0;
     return result;
-}
+};
 
 let countDown = setInterval(() => {
     document.getElementById("countdown").innerHTML = timeTo(confDate).d + " days, " + (timeTo(confDate).h >= 10 ? timeTo(confDate).h : ("0" + timeTo(confDate).h)) + "h " + (timeTo(confDate).m >= 10 ? timeTo(confDate).m : ("0" + timeTo(confDate).m)) + "m " + (timeTo(confDate).s >= 10 ? timeTo(confDate).s : ("0" + timeTo(confDate).s)) + "s until NBTC";
     document.getElementById("earlybird").innerHTML = timeTo(earlyBird).d + " days, " + (timeTo(earlyBird).h >= 10 ? timeTo(earlyBird).h : ("0" + timeTo(earlyBird).h)) + "h " + (timeTo(earlyBird).m >= 10 ? timeTo(earlyBird).m : ("0" + timeTo(earlyBird).m)) + "m " + (timeTo(earlyBird).s >= 10 ? timeTo(earlyBird).s : ("0" + timeTo(earlyBird).s)) + "s left to buy earlybird tickets!";
-    
-}, 1000)
+}, 1000);
